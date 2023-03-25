f958f087c1839f22cb3ec5aa0852947c1aa0c80a
IrynaAx <nnorisonn@gmail.com>
2023-03-25 21:39:30 +0200

    add standard quality background-clip for compatibility

--- js/countdown-sale.js
+++ js/countdown-sale.js
@@ -6,10 +6,10 @@
         const now = Date.now();
         const distance = countDownDate - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h : " + minutes + "m : " + seconds + "s ";
 
